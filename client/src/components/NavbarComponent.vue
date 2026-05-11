ffdba07473b634be7acedf57e16cf7f8d7eb2296
[kiereyes0812] <[reyeskierbrian]>
2026-05-12 02:17:53 +0800

    fix: layout css and logout route

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,51 +1,89 @@
 <script setup>
+//onBeforeMount() is a lifecycle hook much like onMounted(). Lifecycle hooks are functions that are run within a component based on the lifecycle stage of a component.
+import { onBeforeMount, ref, watch } from "vue";
 
-  //onBeforeMount() is a lifecycle hook much like onMounted(). Lifecycle hooks are functions that are run within a component based on the lifecycle stage of a component.
-  import {onBeforeMount, ref, watch} from "vue";
+import { useGlobalStore } from "../stores/global";
 
-  import { useGlobalStore } from "../stores/global";
+import { useRouter } from "vue-router";
+const router = useRouter();
 
-  //Retrieve the global user state from the global store by destructuring it.
-  const {user} = useGlobalStore();
+function handleLogout() {
+  localStorage.removeItem("token");
+  user.email = "";
+  router.push({ name: "Login" });
+}
 
-  console.log(user.email);
-  
-  const email = ref("")
+//Retrieve the global user state from the global store by destructuring it.
+const { user } = useGlobalStore();
 
-  /*     
+console.log(user.email);
+
+const email = ref("");
+
+/*     
     onBeforeMount is used just before the component is mounted on the page. 
     
     This means that it will be executed before the component is visible to the user. 
 
     onBeforeMount usually used to fetching initial data from an API or initializing data that will be used by the component. 
   */
-  // onBeforeMount(()=>{ 
-  //   email.value = localStorage.getItem("email")
-  // });
-
+// onBeforeMount(()=>{
+//   email.value = localStorage.getItem("email")
+// });
 </script>
 
-
-
 <template>
-	<nav class="navbar navbar-expand-lg sticky-top bg-light shadow-sm">
+  <nav class="navbar navbar-expand-lg sticky-top bg-light shadow-sm">
     <div class="container">
       <!-- router-link is used to navigate to pages using the vue Routing system -->
       <!-- This will prevent the page to refresh when changing pages -->
-      <router-link :to="{ name: 'Home' }" class="navbar-brand text-dark fw-bold">Fryktion Training Supply</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <router-link :to="{ name: 'Home' }" class="navbar-brand text-dark fw-bold"
+        >Fryktion Training Supply</router-link
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto">
-          <router-link :to="{ name: 'Products' }" class="nav-link">Products</router-link>
+          <router-link :to="{ name: 'Products' }" class="nav-link"
+            >Products</router-link
+          >
           <!-- <router-link :to="{ name: 'News' }" class="nav-link">News</router-link>     -->
-          <router-link :to="{ name: 'Profile' }" class="nav-link" v-if="user.email">Profile</router-link>   
-         <router-link :to="{ name: 'Register' }" class="nav-link" v-if="!user.email" >Register</router-link>
-          <router-link :to="{ name: 'Login' }" class="nav-link" v-if="!user.email">Login</router-link>
-          <router-link :to="{ name: 'Logout' }" class="nav-link" v-else>Logout</router-link>
+          <router-link
+            :to="{ name: 'Profile' }"
+            class="nav-link"
+            v-if="user.email"
+            >Profile</router-link
+          >
+          <router-link
+            :to="{ name: 'Register' }"
+            class="nav-link"
+            v-if="!user.email"
+            >Register</router-link
+          >
+          <router-link
+            :to="{ name: 'Login' }"
+            class="nav-link"
+            v-if="!user.email"
+            >Login</router-link
+          >
+          <span
+            class="nav-link"
+            style="cursor: pointer"
+            @click="handleLogout"
+            v-else
+            >Logout</span
+          >
         </div>
       </div>
     </div>
- </nav>
+  </nav>
 </template>
